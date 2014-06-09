c10029b34e3de2636f3e182d803b92789693458d
Ngoc Thoai Nguyen <ngocthoaia1@gmail.com>
2014-06-09 17:22:19 +0700

    to-do

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,6 +2,5 @@ class Product < ActiveRecord::Base
   UPDATABLE_COLUMNS_FOR_ADMINS = [:name, :description, :price, :product_line]
   validates :name,          presence: true
   validates :description,   presence: true
-  validates :price,         presence: true
-  validates :product_line,  presence: true
+  mount_uploader :image, ImageUploader
 end
